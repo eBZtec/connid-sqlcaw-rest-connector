60c2fa6e8f20dc0347006c0b2c9f1d0ed2d71b2e
Jean M Santos <jean.michel@ebz.com.br>
2021-12-29 15:46:04 -0300

    added attribute IDC_CTL_SEN_EXN abd removed attribute dsc_senha

--- src/main/java/com/evolveum/polygon/connector/sqlcaw/rest/util/SqlCAWRestConstants.java
+++ src/main/java/com/evolveum/polygon/connector/sqlcaw/rest/util/SqlCAWRestConstants.java
@@ -8,16 +8,17 @@ public interface SqlCAWRestConstants {
 
     /* SqlCAW User attributes */
     String USER_COD_GRUPO = "cod_grupo";
+    String USER_COD_TIPO_USU = "cod_tipo_usu";
     String USER_COD_PARCEIRO = "cod_parceiro";
     String USER_COD_USU = "cod_usu";
     String USER_DAT_ALTERACAO_SENHA = "dat_alteracao_senha";
     String USER_DAT_ULTIMO_ACESSO = "dat_ultimo_acesso";
     String USER_DSC_EMAIL = "dsc_email";
-    String USER_DSC_SENHA = "dsc_senha";
     String USER_FLG_ATIVO = "flg_ativo";
     String USER_FLG_USU_BLOQUEADO = "flg_usu_bloqueado";
     String USER_ID_USU = "id_usu";
     String USER_NOM_USU = "nom_usu";
+    String USER_IDC_CTL_SEN_EXN = "idc_ctl_sen_exn";
     String USER_QTD_SENHA_INVALIDA = "qtd_senha_invalida";
 
     /* Midpoint Operational attributes */
