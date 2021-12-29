f7e48c9eaf7c7b79ed98c1559eb460c0951bbe28
Jean M Santos <jean.michel@ebz.com.br>
2021-12-29 16:25:01 -0300

    added methods for search, translate objects and translate users

--- src/main/java/com/evolveum/polygon/connector/sqlcaw/rest/SqlCAWRestConnector.java
+++ src/main/java/com/evolveum/polygon/connector/sqlcaw/rest/SqlCAWRestConnector.java
@@ -16,21 +16,25 @@
 
 package com.evolveum.polygon.connector.sqlcaw.rest;
 
-import com.evolveum.polygon.connector.sqlcaw.rest.api.RObject;
-import com.evolveum.polygon.connector.sqlcaw.rest.api.RUser;
-import com.evolveum.polygon.connector.sqlcaw.rest.api.TestService;
-import com.evolveum.polygon.connector.sqlcaw.rest.api.UserService;
+import com.evolveum.polygon.connector.sqlcaw.rest.api.*;
 import com.evolveum.polygon.connector.sqlcaw.rest.util.AbstractSqlCAWRestConnector;
+import com.evolveum.polygon.connector.sqlcaw.rest.util.FilterHandler;
 import com.evolveum.polygon.connector.sqlcaw.rest.util.SqlCAWRestConstants;
+import com.evolveum.polygon.connector.sqlcaw.rest.util.StringAccessor;
 import org.apache.cxf.interceptor.Fault;
+import org.identityconnectors.common.CollectionUtil;
+import org.identityconnectors.common.StringUtil;
 import org.identityconnectors.common.logging.Log;
 import org.identityconnectors.common.security.GuardedString;
 import org.identityconnectors.framework.common.exceptions.*;
 import org.identityconnectors.framework.common.objects.*;
+import org.identityconnectors.framework.common.objects.filter.Filter;
+import org.identityconnectors.framework.common.objects.filter.FilterTranslator;
 import org.identityconnectors.framework.spi.Configuration;
 import org.identityconnectors.framework.spi.Connector;
 import org.identityconnectors.framework.spi.ConnectorClass;
 import org.identityconnectors.framework.spi.operations.SchemaOp;
+import org.identityconnectors.framework.spi.operations.SearchOp;
 import org.identityconnectors.framework.spi.operations.TestOp;
 
 import javax.ws.rs.ClientErrorException;
@@ -42,12 +46,11 @@ import java.io.IOException;
 import java.net.ConnectException;
 import java.net.NoRouteToHostException;
 import java.net.SocketTimeoutException;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 @ConnectorClass(displayNameKey = "sqlcawrest.connector.display", configurationClass = SqlCAWRestConfiguration.class)
-public class SqlCAWRestConnector extends AbstractSqlCAWRestConnector implements Connector, TestOp, SchemaOp {
+public class SqlCAWRestConnector extends AbstractSqlCAWRestConnector implements Connector, TestOp, SchemaOp,
+        SearchOp<Filter> {
 
     private static final Log LOG = Log.getLog(SqlCAWRestConnector.class);
 
@@ -138,12 +141,146 @@ public class SqlCAWRestConnector extends AbstractSqlCAWRestConnector implements 
 
     @Override
     protected RUser translateUser(Uid uid, Set<Attribute> attributes) {
+
+        LOG.ok("Translating new User object with the UID {0} to the target resource output type.", uid);
+
+        RUser user = new RUser();
+        translateObject(uid, attributes, user);
+
+        user.setCod_grupo(getAttributeValue(SqlCAWRestConstants.USER_COD_GRUPO, String.class, attributes));
+        user.setCod_parceiro(getAttributeValue(SqlCAWRestConstants.USER_COD_PARCEIRO, String.class, attributes));
+        user.setCod_tipo_usu(getAttributeValue(SqlCAWRestConstants.USER_COD_TIPO_USU, String.class, attributes));
+        user.setCod_usu(getAttributeValue(SqlCAWRestConstants.USER_COD_USU, String.class, attributes));
+        user.setDat_alteracao_senha(getAttributeValue(SqlCAWRestConstants.USER_DAT_ALTERACAO_SENHA,
+                Date.class, attributes));
+        user.setDat_ultimo_acesso(getAttributeValue(SqlCAWRestConstants.USER_DAT_ULTIMO_ACESSO, Date.class,
+                attributes));
+        user.setDsc_email(getAttributeValue(SqlCAWRestConstants.USER_DSC_EMAIL, String.class, attributes));
+        user.setFlg_ativo(getAttributeValue(SqlCAWRestConstants.USER_FLG_ATIVO, String.class, attributes));
+        user.setFlg_usu_bloqueado(getAttributeValue(SqlCAWRestConstants.USER_FLG_USU_BLOQUEADO, String.class,
+                attributes));
+        user.setId_usu(getAttributeValue(SqlCAWRestConstants.USER_ID_USU, String.class, attributes));
+        user.setNom_usu(getAttributeValue(SqlCAWRestConstants.USER_NOM_USU, String.class, attributes));
+        user.setIdc_ctl_sen_exn(getAttributeValue(SqlCAWRestConstants.USER_IDC_CTL_SEN_EXN, String.class, attributes));
+        user.setQtd_senha_invalida(getAttributeValue(SqlCAWRestConstants.USER_QTD_SENHA_INVALIDA, String.class,
+                attributes));
+
+        GuardedString pwd = getAttributeValue(OperationalAttributes.PASSWORD_NAME, GuardedString.class, attributes);
+        if (pwd != null) {
+            StringAccessor accessor = new StringAccessor();
+            pwd.access(accessor);
+            user.setPassword(accessor.getValue());
+        }
+
+        user.setEnabled(getAttributeValue(OperationalAttributes.ENABLE_NAME, Boolean.class, attributes));
+
         return null;
+    }
+
+    private <T> T getAttributeValue(String name, Class<T> type, Set<Attribute> attributes) {
+        LOG.ok("Processing attribute {0} of the type {1}", name, type.toString());
+
+        Attribute attr = AttributeUtil.find(name, attributes);
+
+        if (attr == null) {
+            return null;
+        }
+
+        if (String.class.equals(type)) {
+            return (T) AttributeUtil.getStringValue(attr);
+        } else if (Long.class.equals(type)) {
+            return (T) AttributeUtil.getLongValue(attr);
+        } else if (Integer.class.equals(type)) {
+            return (T) AttributeUtil.getIntegerValue(attr);
+        } else if (GuardedString.class.equals(type)) {
+            return (T) AttributeUtil.getGuardedStringValue(attr);
+        } else if (Boolean.class.equals(type)) {
+            return (T) AttributeUtil.getBooleanValue(attr);
+        } else if (List.class.equals(type)) {
+            return (T) attr.getValue();
+        } else if(Date.class.equals(type)) {
+            return (T) AttributeUtil.getDateValue(attr);
+        } else {
+            throw new InvalidAttributeValueException("Unknown value type " + type);
+        }
+    }
+
+    private void translateObject(Uid uid, Set<Attribute> attributes, RObject object) {
+
+        if (uid != null) {
+            object.setId(uid.getUidValue());
+        }
+
+        String nameUpdate = getAttributeValue(Name.NAME, String.class, attributes);
+
+        Name name;
+
+        if (nameUpdate != null && !nameUpdate.isEmpty()) {
+            name = new Name(nameUpdate);
+        } else {
+            name = AttributeUtil.getNameFromAttributes(attributes);
+        }
+
+        if (name == null || StringUtil.isEmpty(name.getNameValue())) {
+            throw new InvalidAttributeValueException("Name not present or it's empty.");
+        }
+
+        object.setName(name.getNameValue());
     }
 
     @Override
     protected ConnectorObject translate(RObject object) {
-        return null;
+
+        ConnectorObjectBuilder builder = new ConnectorObjectBuilder();
+
+        String objectId = object.getId();
+
+        addAttribute(builder, Uid.NAME, objectId);
+        addAttribute(builder, Name.NAME, object.getName());
+
+        addAttribute(builder, SqlCAWRestConstants.OBJECT_ATTR_CHANGED, object.getChanged());
+
+        if (object instanceof RUser) {
+
+            builder.setObjectClass(ObjectClass.ACCOUNT);
+
+            RUser user = (RUser) object;
+            addAttribute(builder, SqlCAWRestConstants.USER_COD_GRUPO, user.getCod_grupo());
+            addAttribute(builder, SqlCAWRestConstants.USER_COD_PARCEIRO, user.getCod_parceiro());
+            addAttribute(builder, SqlCAWRestConstants.USER_COD_TIPO_USU, user.getCod_tipo_usu());
+            addAttribute(builder, SqlCAWRestConstants.USER_COD_USU, user.getCod_usu());
+            addAttribute(builder, SqlCAWRestConstants.USER_DAT_ALTERACAO_SENHA, user.getDat_alteracao_senha());
+            addAttribute(builder, SqlCAWRestConstants.USER_DAT_ULTIMO_ACESSO, user.getDat_ultimo_acesso());
+            addAttribute(builder, SqlCAWRestConstants.USER_DSC_EMAIL, user.getDsc_email());
+            addAttribute(builder, SqlCAWRestConstants.USER_FLG_ATIVO, user.getFlg_ativo());
+            addAttribute(builder, SqlCAWRestConstants.USER_FLG_USU_BLOQUEADO, user.getFlg_usu_bloqueado());
+            addAttribute(builder, SqlCAWRestConstants.USER_ID_USU, user.getId_usu());
+            addAttribute(builder, SqlCAWRestConstants.USER_IDC_CTL_SEN_EXN, user.getIdc_ctl_sen_exn());
+            addAttribute(builder, SqlCAWRestConstants.USER_NOM_USU, user.getNom_usu());
+            addAttribute(builder, SqlCAWRestConstants.USER_QTD_SENHA_INVALIDA, user.getQtd_senha_invalida());
+            addAttribute(builder, OperationalAttributes.ENABLE_NAME, user.getEnabled());
+        }
+
+        LOG.ok("Object with the Id {0} succesfully build.", objectId);
+        return builder.build();
+    }
+
+    private void addAttribute(ConnectorObjectBuilder builder, String name, Object value) {
+        if (value == null) {
+            return;
+        }
+
+        AttributeBuilder ab = new AttributeBuilder();
+        ab.setName(name);
+
+        if (value instanceof Collection) {
+            ab.addValue((Collection) value);
+        } else {
+            ab.addValue(value);
+        }
+
+        LOG.ok("Attribute: " + name + " with value(s): " + value.toString() + " added to construction.");
+        builder.addAttribute(ab.build());
     }
 
     @Override
@@ -236,5 +373,60 @@ public class SqlCAWRestConnector extends AbstractSqlCAWRestConnector implements 
         }
 
         return aib.build();
+    }
+
+    @Override
+    public FilterTranslator<Filter> createFilterTranslator(ObjectClass objectClass, OperationOptions operationOptions) {
+        return new FilterTranslator<Filter>() {
+            @Override
+            public List<Filter> translate(Filter filter) {
+                return CollectionUtil.newList(filter);
+            }
+        };
+    }
+
+    @Override
+    public void executeQuery(ObjectClass objectClass, Filter filter, ResultsHandler resultsHandler, OperationOptions operationOptions) {
+        String query = "";
+
+        if (filter == null) {
+
+        } else {
+
+            query = filter.accept(new FilterHandler(), "");
+            LOG.info("Query will be executed with the following filter: {0}", query);
+            LOG.info("The object class from which the filter will be executed: {0}", objectClass.getDisplayNameKey());
+        }
+
+        try {
+            RObjects<? extends RObject> objects;
+            if (ObjectClass.ACCOUNT.equals(objectClass)) {
+                objects = userService.list(query);
+            } else {
+                throw new UnsupportedOperationException("Unknown object class " + objectClass);
+            }
+
+            if (objects == null) {
+                LOG.info("No objects returned by query.");
+
+                return;
+            }
+
+            LOG.ok("Objects found: {0}", objects.getObjects().size());
+
+            for (RObject object: objects.getObjects()) {
+                if (object == null) {
+                    continue;
+                }
+
+                ConnectorObject co = translate(object);
+
+                if (!resultsHandler.handle(co)) {
+                    break;
+                }
+            }
+        } catch (Exception ex) {
+            handleGenericException(ex, "Couldn't search " + objectClass + " with filter " + query + ", reason: " + ex.getMessage());
+        }
     }
 }
